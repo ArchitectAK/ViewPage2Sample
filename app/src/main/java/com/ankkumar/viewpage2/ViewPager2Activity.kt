9d0a4da05818a4bccd765d9e4576d5895a5787ca
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-05-12 13:41:00 +0800

    added classes

--- app/src/main/java/com/ankkumar/viewpage2/ViewPager2Activity.kt
+++ app/src/main/java/com/ankkumar/viewpage2/ViewPager2Activity.kt
@@ -2,11 +2,17 @@ package com.ankkumar.viewpage2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 
 class ViewPager2Activity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity)
+        val viewPager = findViewById<ViewPager2>(R.id.view_pager)
+        val userList = arrayListOf("John", "Doe", "Foo", "Bar")
+        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        viewPager.adapter = UserAdapter(userList, supportFragmentManager)
     }
 
 }
